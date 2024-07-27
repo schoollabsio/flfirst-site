db163b5f3b438dfd6fa740942e1679884faa9818
Jeremy School <jschool@Jeremys-MacBook-Pro.local>
2024-07-27 13:20:56 -0400

    add detach false

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker stack deploy -c base.yml -c staging.yml -c env.yml flfirst-site --with-registry-auth
+docker stack deploy -c base.yml -c staging.yml -c env.yml flfirst-site --with-registry-auth --detach=false
